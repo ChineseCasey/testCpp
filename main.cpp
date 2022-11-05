9217da1f997d3785dcd3a238702c53433c978260
ChineseCasey <1036707552@qq.com>
2022-11-05 23:06:20 +0800

    ceshi

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+#include "main.h"
+
+
+void cx::Hello::hello() const {
+    cout << hh << endl;
+}
+
+void cx::Hello::world(int a, int b){
+    if (a != 0 && b != 0)
+    {
+        cout << a+b ;
+    }
+};
+
+
+int main() {
+    //test git
+    cx::Hello hello;
+    int number;
+    number = hello.hh.size();
+    hello.hello();
+    hello.world(number, 1);
+    return 0;
+}
+
